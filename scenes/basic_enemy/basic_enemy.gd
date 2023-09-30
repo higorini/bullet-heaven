e2ecbc9c1030eae784ff23ef6c8b03fd3f8d4285
higorini <108697425+higorini@users.noreply.github.com>
2023-09-30 18:40:17 -0300

    Primeiro skill e inimigo

---
+++ scenes/basic_enemy/basic_enemy.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+const MAX_SPEED: int = 75
+
+
+func _ready():
+	$Area2D.area_entered.connect(on_area_entered)
+
+
+func _process(_delta):
+	var direction = get_direction_to_player()
+	velocity = direction * MAX_SPEED
+	move_and_slide()
+
+
+func get_direction_to_player():
+	var player_nodes = get_tree().get_first_node_in_group("player") as Node2D
+	
+	if player_nodes != null:
+		return (player_nodes.global_position - global_position).normalized()
+		
+	return Vector2.ZERO
+
+
+func on_area_entered(_other_area: Area2D):
+	queue_free()
